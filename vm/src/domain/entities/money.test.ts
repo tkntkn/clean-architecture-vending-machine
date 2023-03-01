683287f55b49a9ad9e55267fd15562fd95b4cd80
tkntkn <kd1049@gmail.com>
2023-03-02 04:12:38 +0900

    Improve test

--- vm/src/domain/entities/money.test.ts
+++ vm/src/domain/entities/money.test.ts
@@ -4,4 +4,5 @@ import { expect, test } from "vitest";
 test("detect 10yen correctly", () => {
   expect(detectMoneyType("10円")).toBeUndefined();
   expect(detectMoneyType("10円玉")).toEqual({ type: MoneyType.Ten, money: "10円玉" });
+  expect(detectMoneyType("10円玉！")).toEqual({ type: MoneyType.Ten, money: "10円玉！" });
 });
